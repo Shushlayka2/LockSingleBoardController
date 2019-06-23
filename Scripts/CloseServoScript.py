f78250941835e866c54b13c9486ba07333f20183
Латыпов Булат Салаватович <BSLatypov@stud.kpfu.ru>
2019-06-23 14:01:09 +0400

    Scripts corrected

--- Scripts/CloseServoScript.py
+++ Scripts/CloseServoScript.py
@@ -1,9 +1,13 @@
 ﻿import OPi.GPIO as GPIO
+import time
 
 GPIO.setboard(GPIO.ZEROPLUS)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 p = GPIO.PWM(7, 50)
+
 p.start(7.5)
 p.ChangeDutyCycle(12.5)
+time.sleep(1)
+p.close()
 GPIO.cleanup()
